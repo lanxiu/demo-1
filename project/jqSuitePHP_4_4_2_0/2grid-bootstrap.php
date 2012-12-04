a17106c1cae367b171b27c57dc509ec7c51eab1d
jnuc093pc <jnuc093@gmail.com>
2012-12-04 23:55:52 +0800

    jquery-ui-bootstrap

---
+++ project/jqSuitePHP_4_4_2_0/2grid-bootstrap.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title> jqGrid bootstrap</title>
+
+	<!--
+	<link rel="stylesheet" type="text/css" media="screen" href="themes/redmond/jquery-ui-custom.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="themes/ui.jqgrid.css" />
+	-->
+	<link type="text/css" href="jquery-ui-bootstrap/css/custom-theme/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
+    <link href="jquery-ui-bootstrap/bootstrap/bootstrap.css" rel="stylesheet">
+
+	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/i18n/grid.locale-en.js" type="text/javascript"></script>
+	<script src="js/jquery.jqGrid.min.js" type="text/javascript"></script>
+
+</head>
+<body>
+	......
+	<?php include "myfirstgrid.php" ?>
+	.......
+</body>
+</html>
